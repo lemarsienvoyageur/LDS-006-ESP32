e93dac9ee30e6404f4efb6c7984a17be2de61bfb
lemarsienvoyageur <97837322+lemarsienvoyageur@users.noreply.github.com>
2022-08-05 15:43:38 +0200

    Add files via upload

---
+++ examples/start_and_stop_serial/start_and_stop_serial.ino
@@ -0,0 +1,28 @@
+/*
+Entry "s" on serial monitor for start lidar and write the data.
+Entry "p" on serial monitor for stop lidar and stop writing data.
+*/
+#include <LDS006ESP32.h>
+
+LDS006 lidar(16, 17, true);
+void setup() {
+
+Serial.begin(115200);
+}
+
+void loop() {
+
+lidar.reading();
+lidar.writing();
+      static byte rr;
+      while (Serial.available() > 0){ 
+      rr = Serial.read();
+      if (rr == 's'){
+      lidar.start();
+    }
+
+    if (rr == 'p'){
+    lidar.pause();
+    }
+      }
+}
